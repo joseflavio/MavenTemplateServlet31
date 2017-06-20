f5b2067d40eb6e6eb947e3d54ce9d1e9a53766d5
Jose Flavio A <joseflavio-at-gmail-dot-com>
2017-06-20 22:22:02 +0200

    Example

---
+++ src/test/java/me/joseflavio/mypackage/TimeServletTest.java
@@ -0,0 +1,11 @@
+package me.joseflavio.mypackage;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TimeServletTest {
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(1, 1);
+    }
+}
