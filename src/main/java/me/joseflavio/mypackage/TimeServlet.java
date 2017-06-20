9e18868cd4929150434ff8b72670a8c31fb5f53c
Jose Flavio A <joseflavio-at-gmail-dot-com>
2017-06-20 22:22:02 +0200

    Example

---
+++ src/main/java/me/joseflavio/mypackage/TimeServlet.java
@@ -0,0 +1,22 @@
+package me.joseflavio.mypackage;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("services/time")
+public class TimeServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.getWriter().write(new Date().toString());
+    }
+
+}
